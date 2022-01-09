2d403aa1b2fdb86c36a6043794e9e64296bf032d
Linmiao Xu <linmiao.xu@gmail.com>
2022-01-09 13:21:54 -0500

    Wrap bsf canvas in a class. Quick way to drag to zoom in

--- js/main.js
+++ js/main.js
@@ -1,46 +1,89 @@
 const max_iterations = 255;
 
+class BurningShipFractalCanvas {
+  canvas;
+  canvasWidth;
+  canvasHeight;
+  xRange;
+  yRange;
+  colorFunc;
+
+  constructor(canvasId, xRange, yRange, colorFunc) {
+    this.canvas = document.getElementById(canvasId);
+    this.canvasWidth = this.canvas.width;
+    this.canvasHeight = this.canvas.height;
+    this.xRange = xRange;
+    this.yRange = yRange;
+    this.colorFunc = colorFunc;
+    this.canvas.addEventListener('mousedown', (event) => {
+      // console.log(`canvas mousedown - ${event.offsetX}, ${event.offsetY}`);
+      // console.dir(event);
+      const x0 =
+        this.xRange[0] * (1 - event.offsetX / this.canvasWidth) +
+        this.xRange[1] * (event.offsetX / this.canvasWidth);
+      const y0 =
+        this.yRange[0] * (1 - event.offsetY / this.canvasHeight) +
+        this.yRange[1] * (event.offsetY / this.canvasHeight);
+      console.log(`(${x0}, ${y0})`);
+      this.xRange[0] = x0;
+      this.yRange[0] = y0;
+    });
+    this.canvas.addEventListener('mouseup', (event) => {
+      // console.log(`canvas mouseup - ${event.offsetX}, ${event.offsetY}`);
+      // console.dir(event);
+      const x1 =
+        this.xRange[0] * (1 - event.offsetX / this.canvasWidth) +
+        this.xRange[1] * (event.offsetX / this.canvasWidth);
+      const y1 =
+        this.yRange[0] * (1 - event.offsetY / this.canvasHeight) +
+        this.yRange[1] * (event.offsetY / this.canvasHeight);
+      this.xRange[1] = x1;
+      this.yRange[1] = y1;
+      this.render();
+    });
+  }
+
+  render() {
+    const context = this.canvas.getContext('2d');
+    const canvasImageData = context.createImageData(this.canvasWidth, this.canvasHeight);
+    const image = canvasImageData.data;
+    for (let i = 0; i < this.canvasHeight; i++) {
+      for (let j = 0; j < this.canvasWidth; j++) {
+        const x0 = this.xRange[0] + j * (this.xRange[1] - this.xRange[0]) / this.canvasWidth;
+        const y0 = this.yRange[0] + i * (this.yRange[1] - this.yRange[0]) / this.canvasHeight;
+        let x = 0;
+        let y = 0;
+        let iteration = 0;
+        while ((x * x + y * y < 4) && (iteration < max_iterations)) {
+          const x_n = x * x - y * y + x0;
+          const y_n = 2 * Math.abs(x * y) + y0;
+          x = x_n;
+          y = y_n;
+          iteration++;
+        }
+        const ind = i * this.canvasWidth * 4 + j * 4;
+        const pixels = this.colorFunc(iteration);
+        for (let p = 0; p < 4; p++) {
+          image[ind + p] = pixels[p];
+        }
+      }
+    }
+    context.putImageData(canvasImageData, 0, 0);
+  }
+}
+
 // let x_range = [-1.88, -1.7];
 // const midpoint = [0.45, 0.5];
 // const range = 1.7;
 // x_range = [midpoint[0] - 2 * range, midpoint[0] +  range];
 // y_range = [midpoint[1] - 2 * range, midpoint[1] +  range];
 
-function drawBurningShipFractal(canvas, x_range, y_range, drawFunc) {
-  const context = canvas.getContext('2d');
-	const canvas_image_data = context.createImageData(canvas.width, canvas.height);
-	const image = canvas_image_data.data;
-  const canvas_height = canvas.height;
-  const canvas_width = canvas.width;
-	for (let i = 0; i < canvas_height; i++) {
-		for (let j = 0; j < canvas_width; j++) {
-			const x0 = x_range[0] + j * (x_range[1] - x_range[0]) / canvas_width;
-			const y0 = y_range[0] + i * (y_range[1] - y_range[0]) / canvas_height;
-			let x = 0;
-			let y = 0;
-			let iteration = 0;
-			while ((x * x + y * y < 4) && (iteration < max_iterations)) {
-				const x_n = x * x - y * y + x0;
-				const y_n = 2 * Math.abs(x * y) + y0;
-				x = x_n;
-				y = y_n;
-				iteration++;
-			}
-      const ind = i * canvas_width * 4 + j * 4;
-      const pixels = drawFunc(iteration);
-      for (let p = 0; p < 4; p++) {
-        image[ind + p] = pixels[p];
-      }
-		}
-	}
-	context.putImageData(canvas_image_data, 0, 0);
-}
 
 function drawCanvases() {
   // console.log(`${JSON.stringify(x_range)} ${JSON.stringify(y_range)}`);
 
-  const canvas1 = document.getElementById('bsf-1');
-  drawBurningShipFractal(canvas1,
+  new BurningShipFractalCanvas(
+    'bsf-1', 
     [-1.8, -1.7],
     [-0.08, 0.01],
     (iteration) => [
@@ -49,17 +92,10 @@ function drawCanvases() {
       85 - iteration * 5,
       255,
     ]
-  );
-  canvas1.addEventListener('mousedown', (event) => {
-    console.log(`canvas mousedown - ${event.offsetX}, ${event.offsetY}`);
-    console.dir(event);
-  });
-  canvas1.addEventListener('mouseup', (event) => {
-    console.log(`canvas mouseup - ${event.offsetX}, ${event.offsetY}`);
-    console.dir(event);
-  });
+  ).render();
 
-  drawBurningShipFractal(document.getElementById('bsf-2'),
+  new BurningShipFractalCanvas(
+    'bsf-2',
     [-1.8, -1.7],
     [-0.08, 0.01],
     (iteration) => [
@@ -68,21 +104,24 @@ function drawCanvases() {
       Math.sin(0.01 * iteration + 1) * 230 + 25,
       255,
     ]
-  );
+  ).render();
 
-  drawBurningShipFractal(document.getElementById('bsf-3'),
+  new BurningShipFractalCanvas(
+    'bsf-3',
     [-1.8, -1.7],
     [-0.08, 0.01],
     (iteration) => [0, 0, 0, iteration],
-  );
+  ).render();
 
-  drawBurningShipFractal(document.getElementById('bsf-large'),
+  new BurningShipFractalCanvas(
+    'bsf-large',
     [-2.5, 1.5],
     [-2, 1],
     (iteration) => [0, 0, 0, iteration],
-  );
+  ).render();
 
-  drawBurningShipFractal(document.getElementById('bsf-wide'),
+  new BurningShipFractalCanvas(
+    'bsf-wide',
     [-2.0, -1.5],
     [-0.09, 0.02],
     (iteration) => [
@@ -91,7 +130,7 @@ function drawCanvases() {
       Math.sin(0.01 * iteration + 1) * 230 + 25,
       255,
     ]
-  );
+  ).render();
 }
 
 {
