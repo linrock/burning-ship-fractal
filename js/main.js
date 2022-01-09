024fc41ba55ddbb6ad0b6ec603258ebd939c03f2
Linmiao Xu <linmiao.xu@gmail.com>
2022-01-09 01:26:04 -0500

    Draw a 3rd canvas, support drawing different coordinates

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const max_iterations = 100;
 
-let x_range = [-1.8, -1.7];
-let y_range = [-0.08, 0.01];
+// let x_range = [-1.88, -1.7];
+// const midpoint = [0.45, 0.5];
+// const range = 1.7;
+// x_range = [midpoint[0] - 2 * range, midpoint[0] +  range];
+// y_range = [midpoint[1] - 2 * range, midpoint[1] +  range];
 
-function drawBurningShipFractal(canvas, drawFunc) {
+function drawBurningShipFractal(canvas, x_range, y_range, drawFunc) {
   const context = canvas.getContext('2d');
 	const canvas_image_data = context.createImageData(canvas.width, canvas.height);
 	const image = canvas_image_data.data;
@@ -37,20 +40,49 @@ function drawCanvases() {
   // console.log(`${JSON.stringify(x_range)} ${JSON.stringify(y_range)}`);
 
   const canvas1 = document.getElementById('bsf-1');
-	drawBurningShipFractal(canvas1, (iteration) => [
-    25 + iteration * 30,
-    25 + iteration * 10,
-    85 - iteration * 5,
-		255,
-  ]);
+  drawBurningShipFractal(canvas1,
+    [-1.8, -1.7],
+    [-0.08, 0.01],
+    (iteration) => [
+      25 + iteration * 30,
+      25 + iteration * 10,
+      85 - iteration * 5,
+      255,
+    ]
+  );
 
   const canvas2 = document.getElementById('bsf-2');
-	drawBurningShipFractal(canvas2, (iteration) => [
-    Math.sin(0.016 * iteration + 4) * 230 + 25,
-    Math.sin(0.013 * iteration + 2) * 230 + 25,
-    Math.sin(0.01 * iteration + 1) * 230 + 25,
-    255,
-  ]);
+  drawBurningShipFractal(canvas2,
+    [-1.8, -1.7],
+    [-0.08, 0.01],
+    (iteration) => [
+      Math.sin(0.016 * iteration + 4) * 230 + 25,
+      Math.sin(0.013 * iteration + 2) * 230 + 25,
+      Math.sin(0.01 * iteration + 1) * 230 + 25,
+      255,
+    ]
+  );
+
+  const canvas3 = document.getElementById('bsf-3');
+  drawBurningShipFractal(canvas3,
+    [-2.0, -1.5],
+    [-0.09, 0.02],
+    (iteration) => [
+      Math.sin(0.016 * iteration + 4) * 230 + 25,
+      Math.sin(0.013 * iteration + 2) * 230 + 25,
+      Math.sin(0.01 * iteration + 1) * 230 + 25,
+      255,
+    ]
+  );
+
+  canvas1.addEventListener('mousedown', (event) => {
+    console.log('canvas mousedown');
+    console.dir(event);
+  });
+  canvas1.addEventListener('mouseup', (event) => {
+    console.log('canvas mouseup');
+    console.dir(event);
+  });
 }
 
 {
