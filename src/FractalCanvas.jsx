7423a9e93356f13165ccbd4633ed79f7689a5fc1
Linmiao Xu <linmiao.xu@gmail.com>
2022-01-17 17:55:03 -0500

    Move fractal math to a separate file

--- src/FractalCanvas.jsx
+++ src/FractalCanvas.jsx
@@ -1,54 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-
-const MAX_ITERATIONS = 255;
-const ESCAPE_RADIUS = 2;
-
-const ESCAPE_THRESHOLD = ESCAPE_RADIUS * ESCAPE_RADIUS;
-const LOG_ESCAPE_RADIUS = Math.log(ESCAPE_RADIUS);
-
-/** Iterate until escape (diverges) or exceeding the max # of iterations */
-function iterateUntilEscape(x0, y0) {
-  let x = 0, y = 0;
-  let iteration = 0;
-  while ((x*x + y*y < ESCAPE_THRESHOLD) && (iteration < MAX_ITERATIONS)) {
-    const x_n = x*x - y*y + x0;
-    const y_n = 2 * Math.abs(x*y) + y0;
-    x = x_n;
-    y = y_n;
-    iteration++;
-  }
-  return [iteration, Math.sqrt(x*x + y*y)];
-}
-
-function getMu(numIterations, escapeDistance) {
-  // https://mathr.co.uk/helm/AtTheHelmOfTheBurningShip-Paper.pdf
-  return numIterations + 1 - Math.log2(Math.log(escapeDistance));
-  // return numIterations + 1 - Math.log(Math.log(escapeDistance) / LOG_ESCAPE_RADIUS);
-}
-
-function drawBurningShipFractal(canvasEl, xRange, yRange, colorFunc) {
-  const canvasWidth = canvasEl.width;
-  const canvasHeight = canvasEl.height;
-  const context = canvasEl.getContext('2d');
-  const canvasImageData = context.createImageData(canvasWidth, canvasHeight);
-  const image = canvasImageData.data;
-  for (let i = 0; i < canvasHeight; i++) {
-    for (let j = 0; j < canvasWidth; j++) {
-      const x0 = xRange[0] + j * (xRange[1] - xRange[0]) / canvasWidth;
-      const y0 = yRange[0] + i * (yRange[1] - yRange[0]) / canvasHeight;
-      const [numIterations, escapeDistance] = iterateUntilEscape(x0, y0);
-      const ind = i * canvasWidth * 4 + j * 4;
-      if (numIterations !== MAX_ITERATIONS) {
-        const mu = getMu(numIterations, escapeDistance);
-        const pixels = colorFunc({ numIterations, mu });
-        for (let p = 0; p < 4; p++) {
-          image[ind + p] = pixels[p];
-        }
-      }
-    }
-  }
-  context.putImageData(canvasImageData, 0, 0);
-}
+import { drawBurningShipFractal } from './fractal_math';
 
 /** Interactive canvas that draws burning ship fractals */
 export function FractalCanvas({ width, height, xRange: xRangeInit, yRange: yRangeInit, colorFunc }) {
@@ -106,9 +57,9 @@ export function FractalCanvas({ width, height, xRange: xRangeInit, yRange: yRang
     });
   }, [canvasElRef, xRange, yRange, colorFunc]);
 
+  // when re-rendering, clear the canvas so the previous render doesn't show through
   useEffect(() => {
     if (!isRendered) {
-      // clear the canvas so the previous drawing doesn't show through
       const canvasEl = canvasElRef.current;
       canvasEl.width = canvasEl.width;   // eslint-disable-line no-self-assign
       canvasEl.height = canvasEl.height; // eslint-disable-line no-self-assign
@@ -199,7 +150,6 @@ export function FractalCanvas({ width, height, xRange: xRangeInit, yRange: yRang
         }}
         onWheel={(event) => {
           // console.log(`wheel! ${event.deltaY}`);
-          event.preventDefault();
           const wheelDirection = event.deltaY > 0 ? 1 : -1;
           const xRangeStep = (xRange[1] - xRange[0]) / 10 * wheelDirection;
           const yRangeStep = (yRange[1] - yRange[0]) / 10 * wheelDirection;
---
+++ src/fractal_math.js
@@ -0,0 +1,53 @@
+const MAX_ITERATIONS = 255;
+const ESCAPE_RADIUS = 2;
+
+const ESCAPE_THRESHOLD = ESCAPE_RADIUS * ESCAPE_RADIUS;
+const LOG_ESCAPE_RADIUS = Math.log(ESCAPE_RADIUS);
+
+/** Iterate until escape (diverges) or exceeding the max # of iterations */
+function iterateUntilEscape(x0, y0) {
+  let x = 0, y = 0;
+  let iteration = 0;
+  while ((x*x + y*y < ESCAPE_THRESHOLD) && (iteration < MAX_ITERATIONS)) {
+    const x_n = x*x - y*y + x0;
+    const y_n = 2 * Math.abs(x*y) + y0;
+    x = x_n;
+    y = y_n;
+    iteration++;
+  }
+  return [iteration, Math.sqrt(x*x + y*y)];
+}
+
+/** re-normalized iteration count */
+function getMu(numIterations, escapeDistance) {
+  // https://mathr.co.uk/helm/AtTheHelmOfTheBurningShip-Paper.pdf
+  // log base 2 assumes an escape radius of 2
+  return numIterations + 1 - Math.log2(Math.log(escapeDistance));
+  // return numIterations + 1 - Math.log(Math.log(escapeDistance) / LOG_ESCAPE_RADIUS);
+}
+
+/** Given a canvas element, draw a fractal using the escape time method */
+export function drawBurningShipFractal(canvasEl, xRange, yRange, colorFunc) {
+  const canvasWidth = canvasEl.width;
+  const canvasHeight = canvasEl.height;
+  const context = canvasEl.getContext('2d');
+  const canvasImageData = context.createImageData(canvasWidth, canvasHeight);
+  const image = canvasImageData.data;
+  for (let i = 0; i < canvasHeight; i++) {
+    for (let j = 0; j < canvasWidth; j++) {
+      const x0 = xRange[0] + j * (xRange[1] - xRange[0]) / canvasWidth;
+      const y0 = yRange[0] + i * (yRange[1] - yRange[0]) / canvasHeight;
+      const [numIterations, escapeDistance] = iterateUntilEscape(x0, y0);
+      const ind = i * canvasWidth * 4 + j * 4;
+      if (numIterations !== MAX_ITERATIONS) {
+        const mu = getMu(numIterations, escapeDistance);
+        const pixels = colorFunc({ numIterations, mu });
+        for (let p = 0; p < 4; p++) {
+          image[ind + p] = pixels[p];
+        }
+      }
+    }
+  }
+  context.putImageData(canvasImageData, 0, 0);
+}
+
